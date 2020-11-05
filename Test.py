b6a6640474632b32f7cdad2f0a1db59a468e1796
WillyB31 <willy.bon1@outlook.fr>
2020-11-05 11:37:22 +0100

    Initial commit

---
+++ Test.py
@@ -0,0 +1,3 @@
+print("Hello World!") #utilisation litéral
+message = "Hello world" # declaration de la variable message en utilisant "="
+print(message) #utilisation de la variable comme argument de la fonction 'print'
